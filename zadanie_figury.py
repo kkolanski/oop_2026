ac204478db81f2ffa94380171dd2d728581e3fff
Karol Kolański <kkolanski.edu@gmail.com>
2026-01-04 13:12:53 +0100

    Added zadanie_figury.py

---
+++ zadanie_figury.py
@@ -0,0 +1,9 @@
+# Napisz klasę FiguraGeometryczna, która będzie zawierała
+# metody:
+# policz_pole()
+# policz_obwód()
+
+# Napisz klasy Prostokąt, Kwadrat, Koło i Trojkat
+# oraz zaimplementuj metody z interfejsu FiguraGeometryczna
+
+# Stwórz instancje tych klas i sprawdź ich działanie
