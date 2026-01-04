5c95cacaee6e2dd5d5f6eeb25317779d806ac46f
Karol Kolański <kkolanski.edu@gmail.com>
2026-01-04 15:04:10 +0100

    Added zadania

--- zadanie_figury.py
+++ zadanie_figury.py
@@ -3,7 +3,35 @@
 # policz_pole()
 # policz_obwód()
 
-# Napisz klasy Prostokąt, Kwadrat, Koło i Trojkat
+
+class FiguraGeometryczna:
+    def policz_pole(self):
+        pass
+    def policz_obwod(self):
+        pass
+
+# Napisz klasę Prostokąt
 # oraz zaimplementuj metody z interfejsu FiguraGeometryczna
 
-# Stwórz instancje tych klas i sprawdź ich działanie
+class Prostokat(FiguraGeometryczna):
+    def __init__(self, a, b):
+        self.a = a
+        self.b = b
+
+    def policz_pole(self):
+        return self.a * self.b
+
+    def policz_obwod(self):
+        return 2*self.a + 2*self.b
+
+class Kwadrat(Prostokat):
+    pass
+
+class Kolo(FiguraGeometryczna):
+    pass
+
+class Trojkat(FiguraGeometryczna):
+    pass
+# Stwórz instancję tej klasy i sprawdź jej działanie
+
+# +Kwadrat, Koło i Trójkąt*
---
+++ zadanie_pizza.py
@@ -0,0 +1,20 @@
+# Korzystając ze stworzonych uprzednio  figur geometrycznych,
+# stwórz klasę Pizza.
+
+# Ma ona bazować na Kole oraz posiadać:
+# cenę
+# metodę liczącą opłacalność
+
+
+# Pizza posiada listę składników
+# Pizza jest w stanie sprawdzić, czy w składnikach znajduje się alergen
+
+
+
+
+
+# USER STORIES:
+# Jako konsument pizzy chciałbym, żeby aplikacja była w stanie policzyć opłacalność pizzy, żeby móc
+# porównywać pizze ze sobą.
+
+# Jako konsument pizzy, chciałbym wiedzieć, czy pizza zawiera alergen na który jestem uczulony.
